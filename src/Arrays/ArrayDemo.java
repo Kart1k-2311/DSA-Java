8b6af7759ab53c216910b7a129eb295c612f6058
Kartik Kumar <kartikx2311@gmail.com>
2026-06-24 18:00:54 +0530

    Add first array program

---
+++ src/Arrays/ArrayDemo.java
@@ -0,0 +1,11 @@
+package Arrays;
+
+public class ArrayDemo {
+    public static void main(String[] args) {
+
+        int[] arr = {12, 45, 67, 23, 89};
+
+        System.out.println(arr[0]);
+        System.out.println(arr[3]);
+    }
+}
